0bc562b8eb94e5432837534ada4576fd10c0ac8b
Raúl Coloma <rau17cb@gmail.com>
2023-12-07 11:52:02 +0100

    update login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,11 +4,18 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import Header from "@/components/header";
+import {Input} from "@nextui-org/input";
+import {Button} from "@nextui-org/button";
+import {EyeFilledIcon, EyeSlashFilledIcon} from "@/components/icons";
+import {Snippet} from "@nextui-org/snippet";
 
 export default function LoginPage() {
     const [errors, setErrors] = useState<string[]>([]);
     const [email, setEmail] = useState<string>("rau17cb@gmail.com");
     const [password, setPassword] = useState<string>("Abc123");
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
     const router = useRouter();
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -30,54 +37,54 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="form-signin w-100 m-auto">
-            <Header/>
-            <form onSubmit={handleSubmit}>
-                <div className={'form-floating'}>
-                    <input
+        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <div className="inline-block max-w-lg text-center justify-center">
+                <Header/>
+                <form onSubmit={handleSubmit}>
+                    <Input
                         type="email"
-                        placeholder="test@test.com"
-                        name="email"
-                        className="form-control mb-2"
+                        label="Email"
                         value={email}
+                        className="form-control mb-2"
                         onChange={(event) => setEmail(event.target.value)}
                     />
-                    <label htmlFor="floatingInput">Email</label>
-                </div>
-                <div className={'form-floating'}>
-                    <input
-                        type="password"
+                    <Input
                         placeholder="123123"
-                        name="password"
+                        label="Contrasenya"
                         className="form-control mb-2"
                         value={password}
                         onChange={(event) => setPassword(event.target.value)}
+                        endContent={
+                            <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                                {isVisible ? (
+                                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                ) : (
+                                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                )}
+                            </button>
+                        }
+                        type={isVisible ? "text" : "password"}
                     />
-                    <label htmlFor='floatingPassword'>Contrassenya</label>
-                </div>
-                <div className="form-check text-start my-3">
-                    <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"/>
-                    <label className="form-check-label" htmlFor="flexCheckDefault">
-                        Remember me
-                    </label>
-                </div>
-                <button
-                    type="submit"
-                    className="btn btn-primary w-100 py-2"
-                >
-                    Accedir
-                </button>
-            </form>
+                    <Button
+                        type="submit"
+                        className="btn btn-primary w-100 py-2"
+                    >
+                        Accedir
+                    </Button>
+                </form>
 
-            {errors.length > 0 && (
-                <div className="alert alert-danger mt-2">
-                    <ul className="mb-0">
+                {errors.length > 0 && (
+                    <div className="mt-8">
                         {errors.map((error) => (
-                            <li key={error}>{error}</li>
+                            <Snippet key={error} hideSymbol hideCopyButton variant="flat" color={'danger'}>
+                                <span>
+                                    {error}
+                                </span>
+                            </Snippet>
                         ))}
-                    </ul>
-                </div>
-            )}
-        </div>
+                    </div>
+                )}
+            </div>
+        </section>
     );
 };
--- components/header.tsx
+++ components/header.tsx
@@ -3,24 +3,8 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <div className="image-header-container">
+        <div>
             <Image src={logo.src} alt={"logo"} width={400} height={100} className="mt-5 mb-5" />
         </div>
-        // <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        //     <div className="container-fluid">
-        //         <Link href="/" className="navbar-brand">Home
-        //         </Link>
-        //         <div>
-        //             <ul className="navbar-nav me-auto mb-2 mb-md-0">
-        //                 <li className="nav-item">
-        //                     <Link href="/login" className="nav-link active">Login</Link>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <Link href="/register" className="nav-link active">Register</Link>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
     )
 }
