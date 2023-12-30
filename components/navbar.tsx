ea2c7fadfa880096645fa6644a70a39db05d47d0
Raúl Coloma <rau17cb@gmail.com>
2023-12-30 21:05:42 +0100

    add navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,45 +10,19 @@ import {
 	NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
 
 import { siteConfig } from "@/config/site";
 import NextLink from "next/link";
 
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-	GithubIcon,
-	SearchIcon,
-} from "@/components/icons";
+import { GithubIcon } from "@/components/icons";
 
 import { Logo } from "@/components/icons";
 import {useSession, signOut, signIn} from "next-auth/react";
 
 export const Navbar = () => {
 	const session = useSession();
-
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			endContent={
-				<Kbd className="hidden lg:inline-block" keys={["command"]}>
-					K
-				</Kbd>
-			}
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
 
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky">
@@ -80,35 +54,18 @@ export const Navbar = () => {
 				</NavbarItem>
 			</NavbarContent>
 			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-				<Link isExternal href={siteConfig.links.github} aria-label="Github">
-					<GithubIcon className="text-default-500" />
-				</Link>
 				<ThemeSwitch />
-				<NavbarMenuToggle />
+				{session !== null
+					? <NavbarItem className="lg:flex">{session.data?.user.name}</NavbarItem>
+					: <NavbarItem className="lg:flex"></NavbarItem>
+				}
+				<NavbarItem className="md:flex">
+					{session.data !== null
+						? <Button onClick={() => signOut()} color={'danger'}>Log out</Button>
+						: <Button onClick={() => signIn()} color={'primary'}>Log in</Button>
+					}
+				</NavbarItem>
 			</NavbarContent>
-
-			<NavbarMenu>
-				{searchInput}
-				<div className="mx-4 mt-2 flex flex-col gap-2">
-					{siteConfig.navMenuItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
-								}
-								href="#"
-								size="lg"
-							>
-								{item.label}
-							</Link>
-						</NavbarMenuItem>
-					))}
-				</div>
-			</NavbarMenu>
 		</NextUINavbar>
 	);
 };
